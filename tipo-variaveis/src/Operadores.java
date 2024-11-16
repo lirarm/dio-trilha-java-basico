092609bd6e6882403742b224eaecb8291fd4ca14
RICARDO MOREIRA LIRA <lirarm@hotmail.com>
2024-11-16 10:19:30 -0300

    alteracao tipo variaveis

---
+++ tipo-variaveis/src/Operadores.java
@@ -0,0 +1,8 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "RICARDO MOREIRA LIRA";
+
+        System.out.println(nomeCompleto);
+    }
+    
+}
---
+++ tipo-variaveis/src/TipoVariaveis.java
@@ -0,0 +1,13 @@
+public class TipoVariaveis {
+    public static void main(String[] args) {
+        String nomeCompleto = "Ricardo Moreira Lira";
+
+        int numeroCasa = 340;
+        long CPF = 79334644168L;
+        double salarioMinimo = 1500.00;
+        date dataNascimento = new date();
+
+
+
+    }
+}
