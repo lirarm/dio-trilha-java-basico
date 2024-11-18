0331b806f64cfb6cbbca310896a4eb5536551082
RICARDO MOREIRA LIRA <lirarm@hotmail.com>
2024-11-18 10:04:35 -0300

    insercao codigo programa conta terminal

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor digite o número da sua Conta: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor digite o número da sua Agencia: ");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor digite o seu nome completo: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Por favor digite o valor do seu depósito: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
